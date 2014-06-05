b5e9e7a8dde2644146a6d1e2f73555bf32f62a9c
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2014-06-05 11:54:29 +0900

    Remote list page.

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -16,6 +16,49 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+var loadFeeds = function() {
+    Ext.setup({
+        onReady: function() {
+            var myMask = new Ext.LoadMask(Ext.getBody(), {msg:"Please wait..."});
+            myMask.show();
+
+            var setList = function(data) {
+                Ext.create('Ext.List', {
+                    fullscreen: true,
+
+                    store: {
+                        fields: ['title', 'link'],
+                        data: data
+                    },
+
+                    itemTpl: '{title}',
+
+                    listeners: {
+                        select: function(view, record) {
+                            navigator.app.loadUrl(record.get('link'), {openExternal: true});
+                            //Ext.Msg.alert('Selected!', 'You selected ' + record.get('link'));
+                        }
+                    }
+                });
+            };
+            
+            Ext.data.JsonP.request({
+                url: 'http://apps.mrlatte.net/api/feeds.json',
+                callbackKey: 'callback',
+                method: 'GET',
+                params: {
+                    url: 'http://blog.mrlatte.net/feeds/posts/default'
+                },
+                callback: function(successful, data) {
+                    setList(data.output.responseData.feed.entries);
+                    myMask.hide();
+                }
+            });
+        }
+    });
+};
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -43,9 +86,10 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+        parentElement.querySelector('.listening').setAttribute('style', 'display:none;');
+        
         console.log('Received Event: ' + id);
-
-        location.href = './pages/list.html';
+        
+        loadFeeds();
     }
 };
