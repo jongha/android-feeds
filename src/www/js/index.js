aaea6cd2b8a9f6be76f95ac75443dd91bcfd6a06
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2014-06-07 17:51:34 +0900

    pullrefresh plugin to file. remove comment codes.

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -1,61 +1,103 @@
+var tags = 'software';
+var loadFeed = function(tags, config) {
+    config = config || { masked: true, callback: null };
+
+    if(config.masked) {
+        Ext.Viewport.setMasked(
+            {
+                xtype: 'loadmask',
+                message: 'Please wait...'
+            }
+        );
+    }
+
+    Ext.data.JsonP.request({
+        url: 'http://apps.mrlatte.net/api/feeds.json',
+        callbackKey: 'callback',
+        method: 'GET',
+        params: {
+            tags: tags
+        },
+
+        callback: function(successful, data) {
+            if(config && config.callback) {
+                config.callback(data);
+            }
+
+            //for(var i=pnl.getItems().length-1; i>1; --i) {
+            //    pnl.removeAt(i);
+            //}
+
+            if(config.masked) {
+                Ext.Viewport.setMasked(false);
+            }
+        }
+    });
+};
+
 var pageLoad = function() {
     Ext.setup({
 
         onReady: function() {
-            var tags = 'software,rss';
-            
             var store = Ext.create('Ext.data.Store', {
                 fields: ['title', 'link', 'author', 'publishedDate'],
                 data: [],
                 grouper: {
                     groupFn: function(record) {
-                        return prettyDate(record.get('publishedDate'));
+                        return (new Date(record.get('publishedDate'))).toDateString();//prettyDate(record.get('publishedDate'));
                     }
-                }
-            });
-
-            Ext.define('Feeds.plugin.PullRefresh', {
-                extend: 'Ext.plugin.PullRefresh',
-                fetchLatest: function() {
-                    var that = this;
-                    loadFeed(
-                        tags,
-                        {
-                            masked: false,
-                            callback: function(data) {
-                                var store = that.getList().getStore(),
-                                    proxy = store.getProxy(),
-                                    operation;
-
-                                store.loadData([],false);
-                                for(var i=0; i<data.output.length; ++i) {
-                                    store.add(data.output[i].responseData.feed.entries);
-                                }
-
-                                operation = Ext.create('Ext.data.Operation', {
-                                    page: 1,
-                                    start: 0,
-                                    model: store.getModel(),
-                                    limit: store.getPageSize(),
-                                    action: 'read',
-                                    sorters: store.getSorters(),
-                                    filters: store.getRemoteFilter() ? store.getFilters() : []
-                                });
-
-                                proxy.read(operation, that.onLatestFetched, that);
-                            }
-                        }
-                    );
                 },
+                sort: [
+                    { property: 'publishedDate', direction: 'DESC' },
+                    { property: 'title', direction: 'ASC' },
+                ],
             });
 
-            Ext.create('Ext.dataview.List', {
+            Ext.create('Ext.dataview.List', {            
                 fullscreen: true,
                 grouped: true,
 
                 store: store,
 
-                itemTpl: '<tpl for="."><h1>{title}</h1><h2>{author}</h2><small>{publishedDate}</small></tpl>',
+                itemTpl: new Ext.XTemplate(
+                    '<tpl for=".">',
+                    '<h1>{title}</h1>',
+                    '<tpl if="author && publishedDate"><span><small>{author}</small>, <small>{publishedDate}</small></span></tpl>',
+                    '</tpl>',
+                    {
+                    }
+                ),
+
+                /*
+                //set the function when a user taps on a disclsoure icon
+                onItemDisclosure: function(record, item, index, e) {
+                    //show a messagebox alert which shows the persons firstName
+                    e.stopEvent();
+                    //Ext.Msg.alert('Disclose', 'Disclose more info for ' + record.get('firstName'));
+                },
+                */
+
+                items: [{
+                    xtype: 'titlebar',
+                    docked: 'top',
+                    title: 'Feeds',
+                    items: [
+                        /*{
+                            iconCls: 'more',
+                            align: 'left',
+                            handler: function() {
+                                refresh(true);
+                            },
+                        },*/
+                        {
+                            iconCls: 'refresh',
+                            align: 'right',
+                            handler: function() {
+                                refresh(true);
+                            },
+                        }                        
+                    ]
+                }],
 
                 listeners: {
                     itemtap: function (list, index, item, record, senchaEvent) {
@@ -65,59 +107,31 @@ var pageLoad = function() {
 
                 plugins: [
                     {
-                        xclass: 'Feeds.plugin.PullRefresh',
+                        xclass: 'js.feeds.plugin.pullrefresh',
                     }
                 ],
             });
 
 
-            var loadFeed = function(tags, config) {
-                config = config || { masked: true, callback: null };
+            var refresh = function(removeAll) {
+                loadFeed(
+                    tags,
+                    {
+                        masked: true,
+                        callback: function(data) {
+                            if(removeAll) {
+                                store.loadData([],false);
+                            }
 
-                if(config.masked) {
-                    Ext.Viewport.setMasked(
-                        {
-                            xtype: 'loadmask',
-                            message: 'Please wait...'
-                        }
-                    );
-                }
-
-                Ext.data.JsonP.request({
-                    url: 'http://apps.mrlatte.net/api/feeds.json',
-                    callbackKey: 'callback',
-                    method: 'GET',
-                    params: {
-                        tags: tags
-                    },
-
-                    callback: function(successful, data) {
-                        if(config && config.callback) {
-                            config.callback(data);
-                        }
-
-                        //for(var i=pnl.getItems().length-1; i>1; --i) {
-                        //    pnl.removeAt(i);
-                        //}
-
-                        if(config.masked) {
-                            Ext.Viewport.setMasked(false);
+                            for(var i=0; i<data.output.length; ++i) {
+                                store.add(data.output[i].responseData.feed.entries);
+                            }
                         }
                     }
-                });
+                );
             };
 
-            loadFeed(
-                tags,
-                {
-                    masked: true,
-                    callback: function(data) {
-                        for(var i=0; i<data.output.length; ++i) {
-                            store.add(data.output[i].responseData.feed.entries);
-                        }
-                    }
-                }
-            );
+            refresh();
         }
     });
 };
